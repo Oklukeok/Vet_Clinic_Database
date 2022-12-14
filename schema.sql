2a514203dbbe51aeecde061fd96c9705bf850857
Lucas Bermudez <lucasabermudez@gmail.com>
2022-12-14 17:00:02 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,3 +3,5 @@
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), date_of_birth DATE, escape_attempts INT, neutered BIT, weight_kg DECIMAL, PRIMARY KEY(id)
 );
+/* Added species column to animals table*/
+ALTER TABLE animals ADD COLUMN species VARCHAR(250);
