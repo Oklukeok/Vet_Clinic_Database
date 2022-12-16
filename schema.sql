065db679004957c56748cecb3a4310f172b639ca
Lucas Bermudez <lucasabermudez@gmail.com>
2022-12-15 19:58:27 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -5,3 +5,13 @@ CREATE TABLE animals (
 );
 /* Added species column to animals table*/
 ALTER TABLE animals ADD COLUMN species VARCHAR(250);
+
+/*Project 3*/
+CREATE TABLE owners( id INT GENERATED ALWAYS AS IDENTITY, full_name VARCHAR(250), age INT, PRIMARY KEY(id) );
+CREATE TABLE species( id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), PRIMARY KEY(id) );
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species (id);
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners (id);
+
