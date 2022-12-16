b82a1978300347feac0136f311f7b5440cc60ad1
Lucas Bermudez <lucasabermudez@gmail.com>
2022-12-16 11:33:24 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -52,3 +52,18 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT SUM(escape_attempts),neutered FROM animals WHERE neutered='1' GROUP BY neutered UNION ALL SELECT SUM(escape_attempts),neutered FROM animals WHERE neutered='0' GROUP BY neutered;
 SELECT species, MAX(weight_kg) AS WEIGHT FROM animals WHERE species='pokemon' GROUP BY species UNION ALL SELECT species, MIN(weight_kg) FROM animals WHERE species='pokemon' GROUP BY species UNION ALL SELECT species, MAX(weight_kg) AS WEIGHT FROM animals WHERE species='digimon' GROUP BY species UNION ALL SELECT species, MIN(weight_kg) FROM animals WHERE species='digimon' GROUP BY species ;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+
+
+
+/* Project 3 queries */
+
+
+SELECT * FROM animals INNER JOIN owners ON animals.owner_id = owners.id WHERE full_name= 'Melody Pond';
+SELECT * FROM animals INNER JOIN species ON animals.species_id = species.id WHERE species.name= 'Pokemon';
+SELECT * FROM owners LEFT JOIN animals on owners.id = animals.owner_id;
+SELECT species.name, COUNT(animals.species_id) FROM species INNER JOIN animals ON species.id=animals.species_id GROUP BY species.name;
+SELECT * FROM animals as Animals JOIN species as Species ON Species.id = Animals.species_id JOIN owners as Owners ON Owners.id = Animals.owner_id WHERE Species.name= 'Digimon' AND Owners.full_name = 'Jennifer Orwell';
+SELECT * FROM animals as Animals JOIN owners as Owners ON Owners.id = Animals.owner_id WHERE Animals.escape_attempts= 0 AND Owners.full_name= 'Dean Winchester';
+SELECT Animals.owner_id, Owners.full_name FROM animals as Animals INNER JOIN owners as Owners ON Animals.owner_id= Owners.id GROUP BY Animals.owner_id, Owners.full_name
+ORDER BY COUNT(*) DESC LIMIT 1;
