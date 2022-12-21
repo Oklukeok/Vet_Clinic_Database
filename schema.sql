63c4454c85bcd3e97cf0630d327b6964d8de5105
Lucas Bermudez <lucasabermudez@gmail.com>
2022-12-21 08:52:21 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -22,3 +22,7 @@ CREATE TABLE vets ( id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(250), age 
 CREATE TABLE specializations(species_id INT, vets_id INT, PRIMARY KEY (species_id, vets_id), FOREIGN KEY(species_id) REFERENCES species (id), FOREIGN KEY (vets_id) REFERENCES vets (id));
 
 CREATE TABLE visits (animals_id INTEGER NOT NULL, vets_id INTEGER NOT NULL, visit_date DATE, FOREIGN KEY (animals_id) REFERENCES animals (id), FOREIGN KEY (vets_id) REFERENCES vets (id));
+
+/* PROJECT 5 */
+
+ALTER TABLE owners ADD COLUMN email VARCHAR(120);
